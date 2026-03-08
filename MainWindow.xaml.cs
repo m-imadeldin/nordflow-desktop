e93f7c8b56b5e8e96e9a5ccfdf874e3002da899a
m-imadeldin <mohammed.imadeldin@medieinstitutet.se>
2026-03-08 23:29:38 +0100

    Add edit customer functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,54 +1,75 @@
-﻿using System.Collections.ObjectModel;
-using NordFlow.Services;
-using System.Text;
-using System.Windows;
-using System.Collections.Generic;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using NordFlow.Models;
+﻿    using System.Collections.ObjectModel;
+    using NordFlow.Services;
+    using System.Text;
+    using System.Windows;
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+    using NordFlow.Models;
 
-namespace NordFlow;
+    namespace NordFlow;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
-{
-    private CustomerService _customerService = new CustomerService();
-    private ObservableCollection<Customer> _customers;
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        _customers = new ObservableCollection<Customer>(_customerService.GetCustomers());
-        CustomerListBox.ItemsSource = _customers;
-    }   
-    
-    /// Button
-    public void MyButton_Click(object sender, RoutedEventArgs e)
-    {
-        if (string.IsNullOrWhiteSpace(NameInput.Text)) return; 
+        private CustomerService _customerService = new CustomerService();
+        private ObservableCollection<Customer> _customers;
+        private Customer _selectedCustomer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _customers = new ObservableCollection<Customer>(_customerService.GetCustomers());
+            CustomerListBox.ItemsSource = _customers;
+        }   
         
-        var newCustomer = new Customer { Name = NameInput.Text };
-        _customers.Add(newCustomer);
-        NameInput.Text = "";
-
-
-        
-        
-    }
-    
-            public void DeleteButton_Click(object sender, RoutedEventArgs e)
+        /// Add Button
+        public void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameInput.Text)) return; 
+            
+            if ((_selectedCustomer) != null)
             {
-                var selected = CustomerListBox.SelectedItem as Customer;
-                if (selected == null) return;
+                _selectedCustomer.Name = NameInput.Text;
+                CustomerListBox.Items.Refresh();
+                NameInput.Text = "";
+                _selectedCustomer = null;
+                return;
 
-                _customers.Remove(selected);
             }
-    
-}
+            var newCustomer = new Customer { Name = NameInput.Text };
+            _customers.Add(newCustomer);
+            
+        }
+        //Remove customer button
+                public void DeleteButton_Click(object sender, RoutedEventArgs e)
+                {
+                    var selected = CustomerListBox.SelectedItem as Customer;
+                    if (selected == null) return;
+
+                    _customers.Remove(selected);
+                }
+        
+        //Edit customer button
+
+        public void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = CustomerListBox.SelectedItem as Customer;
+            if (selected == null) return;
+            
+            NameInput.Text = selected.Name;
+            _selectedCustomer = selected;
+
+
+
+
+
+        }
+    }
