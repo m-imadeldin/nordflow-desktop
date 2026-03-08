f797d499461c07e5d32b32c30fd92ccc1bb20fe0
m-imadeldin <mohammed.imadeldin@medieinstitutet.se>
2026-03-08 01:17:47 +0100

    set up NordFlow WPF app with button click interaction

---
+++ Services/CustomerService.cs
@@ -0,0 +1,16 @@
+using NordFlow.Models;
+
+namespace NordFlow.Services;
+
+public class CustomerService
+{
+    public List<Customer> GetCustomers()
+    {
+        return new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Anna Svensson", Email = "anna@gmail.com" },
+            new Customer { Id = 2, Name = "Erik Johansson", Email = "erik@gmail.com" },
+            new Customer { Id = 3, Name = "Sara Ali", Email = "sara@gmail.com" }
+        };
+    }
+}
